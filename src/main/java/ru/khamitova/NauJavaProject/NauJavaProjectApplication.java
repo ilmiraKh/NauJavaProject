96d7bde3719e84db04235ca6eb287f9a88abb765
ilmiraKhamitova <miragge.k@gmail.com>
2025-10-14 08:03:55 +0300

    init Spring Boot

---
+++ src/main/java/ru/khamitova/NauJavaProject/NauJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.khamitova.NauJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/khamitova/NauJavaProject/NauJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khamitova.NauJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
